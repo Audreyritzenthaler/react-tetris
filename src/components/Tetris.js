6b48d58cb75f231e5ff1b40ecc200b09bd6d0c09
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-09-06 19:24:02 +0200

    add checkcollision to the side and down, game over if so many tetrominos are on the top

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -3,7 +3,7 @@ import Stage from './Stage'
 import Display from './Display'
 import StartButton from './StartButton'
 
-import { createStage } from '../gameHelpers'
+import { createStage, checkCollision } from '../gameHelpers'
 import { usePlayer } from '../hook/usePlayer'
 import { useStage} from '../hook/useStage'
 
@@ -15,23 +15,35 @@ const Tetris = () => {
     const [gameOver, setGameOver] = useState(false)
 
     const [player, updatePlayerPos, resetPlayer] = usePlayer()
-    const [stage, setStage] = useStage(player)
+    const [stage, setStage] = useStage(player, resetPlayer)
 
     console.log('re-render')
 
     const movePlayer = (dir) => {
-        updatePlayerPos({ x: dir , y: 0})
+        if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+            updatePlayerPos({ x: dir , y: 0})
+        }
     }
 
     const startGame = () => {
+        console.log('test')
         setStage(createStage())
         resetPlayer()
+        setGameOver(false)
     }
     
     const drop = () => {
-        updatePlayerPos({ x: 0, y:1, collided: false})
+        if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+            updatePlayerPos({ x: 0, y: 1, collided: false })
+        } else {
+            if (player.pos.y < 1) {
+                console.log('GameOver')
+                setGameOver(true)
+                setDropTime(null)
+            }
+            updatePlayerPos({ x: 0, y: 0 , collided: true})
+        }
     }
-
     const dropPlayer = () => {
         drop()
     }
--- src/hook/useStage.js
+++ src/hook/useStage.js
@@ -1,30 +1,37 @@
-import { useState, useEffect } from 'react'
-import { createStage } from '../gameHelpers'
+import { useState, useEffect } from 'react';
+import { createStage } from '../gameHelpers';
 
 export const useStage = (player, resetPlayer) => {
-    const [stage, setStage] = useState(createStage())
+  const [stage, setStage] = useState(createStage());
 
-    useEffect(() => {
-        const updateStage = prevStage => {
-            // First flush the stage
-            const newStage = prevStage.map(row =>
-                row.map(cell =>(cell[1] === 'clear' ? [0, 'clear'] : cell)),
-            )
-            player.tetromino.forEach((row, y) => {
-                row.forEach((value, x) => {
-                    if (value !== 0) {
-                        newStage[y + player.pos.y][x + player.pos.x] = [
-                            value,
-                            `${player.collided ? 'merged' : 'clear'}`
-                        ]
-                    }
-                })
-            })
-            return newStage
-        }
+  useEffect(() => {
+    const updateStage = prevStage => {
+      // First flush the stage
+      const newStage = prevStage.map(row =>
+        row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell)),
+      );
 
-        setStage(prev => updateStage(prev))
-    }, [player])
+      // Then draw the tetromino
+      player.tetromino.forEach((row, y) => {
+        row.forEach((value, x) => {
+          if (value !== 0) {
+            newStage[y + player.pos.y][x + player.pos.x] = [
+              value,
+              `${player.collided ? 'merged' : 'clear'}`,
+            ];
+          }
+        });
+      });
+      // Then check if we collided
+      if (player.collided) {
+        resetPlayer();
+      }
 
-    return [stage, setStage]
-}
+      return newStage;
+    };
+
+    setStage(prev => updateStage(prev));
+  }, [player, resetPlayer]);
+
+  return [stage, setStage];
+};
